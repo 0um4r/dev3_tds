0e4a61aa83c6b3869624d911e7dd13ecc16d162a
54516 <54516@etu.he2b.be>
2021-12-29 19:42:09 +0100

    :construction: Ex 1.2 : produire affichage avec la fonction isPrime()

--- C/td1/main.c
+++ C/td1/main.c
@@ -3,5 +3,17 @@
 
 int main()
 {
-    printf(isPrime(8) ? "true" : "false");
+    //printf(isPrime(8) ? "true" : "false");
+
+    //######### Ex 1.2 #########
+    printf("Les nombres premiers entre 200 et 349 :\n");
+    unsigned newLine = 0;
+    for(unsigned i = 200; i <= 349; i++) {
+        if(newLine == 10) {
+            printf("\n");
+            newLine = 0;
+        }
+        isPrime(i) ? printf("%d", i) : printf(" . ");
+        newLine++;
+    }
 }
