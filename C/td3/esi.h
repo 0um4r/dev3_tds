b324865ef598712c60707a8eebbea0588899ca69
54516 <54516@etu.he2b.be>
2021-12-30 19:32:59 +0100

    :pencil2: Ex 3.1 : corrections d'erreurs

--- C/td3/esi.h
+++ C/td3/esi.h
@@ -6,15 +6,15 @@
 
 /**
  * @brief xstrlen retourne le nombre de caractères d'une chaîne de caractère
- * @param chaine pointeur constant de char
+ * @param chaine pointeur de char constant
  * @return le nombre de caractères d'une chaîne de caractère
  */
 size_t xstrlen(const char * chaine);
 
 /**
  * @brief xstrcmp compare 2 chaînes de caractères lexicographiquement
- * @param lhs pointeur constant de char
- * @param rhs pointeur constant de char
+ * @param lhs pointeur de char constant
+ * @param rhs pointeur de char constant
  * @return une valeur négative si lhs apparaît avant rhs, zéro si égaux sinon positive
  */
 int xstrcmp(const char * lhs, const char * rhs);
